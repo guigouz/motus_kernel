503c2bd203347d1eafa8f43daa82733dd5251d2c
Alfred Wanga <awanga@gmail.com>
2014-07-21 13:10:13 -0400

    msm: arch reset function fix

--- arch/arm/mach-msm/pm.c
+++ arch/arm/mach-msm/pm.c
@@ -695,7 +695,7 @@ static void msm_pm_power_off(void)
 	for (;;) ;
 }
 
-static void msm_pm_restart(char str)
+static void msm_pm_restart(char str, const char *cmd)
 {
 	msm_proc_comm(PCOM_RESET_CHIP, &restart_reason, 0);
 
