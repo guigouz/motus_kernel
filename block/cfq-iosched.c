d33d4161508451d441d618a86cd450ab7d80694d
Alfred Wanga <awanga@gmail.com>
2016-12-14 19:45:04 -0500

    cfq: Remove wait_request flag when idle time is being deleted
    Remove wait_request flag when idle time is being deleted, otherwise
    it'll hit this path every time when a request is enqueued.
    
    Signed-off-by: Gui Jianfeng <guijianfeng@cn.fujitsu.com>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -3219,9 +3219,10 @@ cfq_rq_enqueued(struct cfq_data *cfqd, struct cfq_queue *cfqq,
 			if (blk_rq_bytes(rq) > PAGE_CACHE_SIZE ||
 			    cfqd->busy_queues > 1) {
 				del_timer(&cfqd->idle_slice_timer);
-				blk_start_queueing(cfqd->queue);
-			}
-			cfq_mark_cfqq_must_dispatch(cfqq);
+				cfq_clear_cfqq_wait_request(cfqq);
+				__blk_run_queue(cfqd->queue);
+			} else
+				cfq_mark_cfqq_must_dispatch(cfqq);
 		}
 	} else if (cfq_should_preempt(cfqd, cfqq, rq)) {
 		/*
