fe1aab8b09ad7a8714e4455edc045ce8b541923d
Alfred Wanga <awanga@gmail.com>
2016-12-28 10:10:39 -0500

    ACPI: WMI: Survive BIOS with duplicate GUIDs
    It would appear that in BIOS's with nVidia hooks, the GUID
    05901221-D566-11D1-B2F0-00A0C9062910 is duplicated. For now, the simplest
    solution is to just ignore any duplicate GUIDs. These particular hooks are not
    currently supported/ used in the kernel, so whoever does that can figure out
    what the 'right' solution should be (if there's a better one).
    
    http://bugzilla.kernel.org/show_bug.cgi?id=14846
    
    Signed-off-by: Carlos Corbacho <carlos@strangeworlds.co.uk>
    Reported-by: Larry Finger <Larry.Finger@lwfinger.net>
    Reported-by: Oldřich Jedlička <oldium.pro@seznam.cz>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/char/ipmi/ipmi_si_intf.c
+++ drivers/char/ipmi/ipmi_si_intf.c
@@ -3209,7 +3209,7 @@ static __devinit int init_ipmi_si(void)
 #ifdef CONFIG_ACPI
 	spmi_find_bmc();
 #endif
-#ifdef CONFIG_PNP
+#ifdef CONFIG_ACPI
 	pnp_register_driver(&ipmi_pnp_driver);
 #endif
 
@@ -3335,7 +3335,7 @@ static __exit void cleanup_ipmi_si(void)
 #ifdef CONFIG_PCI
 	pci_unregister_driver(&ipmi_pci_driver);
 #endif
-#ifdef CONFIG_PNP
+#ifdef CONFIG_ACPI
 	pnp_unregister_driver(&ipmi_pnp_driver);
 #endif
 
