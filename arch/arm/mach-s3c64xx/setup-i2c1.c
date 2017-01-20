caacac768f706dc9646924d0cbcbf518df54bca7
Alfred Wanga <awanga@gmail.com>
2017-01-20 06:53:41 -0500

    ARM: S3C64XX: Move device and device setup into mach-s3c64xx
    Move the S3C64XX specific device and setup files into mach-s3c64xx as
    they are unlikely to be used outside of this code.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s3c64xx/setup-i2c1.c
+++ arch/arm/mach-s3c64xx/setup-i2c1.c
@@ -1,11 +1,11 @@
-/* linux/arch/arm/plat-s3c64xx/setup-i2c0.c
+/* linux/arch/arm/plat-s3c64xx/setup-i2c1.c
  *
  * Copyright 2008 Openmoko, Inc.
  * Copyright 2008 Simtec Electronics
  *	Ben Dooks <ben@simtec.co.uk>
  *	http://armlinux.simtec.co.uk/
  *
- * Base S3C64XX I2C bus 0 gpio configuration
+ * Base S3C64XX I2C bus 1 gpio configuration
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -18,14 +18,14 @@
 struct platform_device; /* don't need the contents */
 
 #include <mach/gpio.h>
+#include <mach/gpio-bank-b.h>
 #include <plat/iic.h>
-#include <plat/gpio-bank-b.h>
 #include <plat/gpio-cfg.h>
 
-void s3c_i2c0_cfg_gpio(struct platform_device *dev)
+void s3c_i2c1_cfg_gpio(struct platform_device *dev)
 {
-	s3c_gpio_cfgpin(S3C64XX_GPB(5), S3C64XX_GPB5_I2C_SCL0);
-	s3c_gpio_cfgpin(S3C64XX_GPB(6), S3C64XX_GPB6_I2C_SDA0);
-	s3c_gpio_setpull(S3C64XX_GPB(5), S3C_GPIO_PULL_UP);
-	s3c_gpio_setpull(S3C64XX_GPB(6), S3C_GPIO_PULL_UP);
+	s3c_gpio_cfgpin(S3C64XX_GPB(2), S3C64XX_GPB2_I2C_SCL1);
+	s3c_gpio_cfgpin(S3C64XX_GPB(3), S3C64XX_GPB3_I2C_SDA1);
+	s3c_gpio_setpull(S3C64XX_GPB(2), S3C_GPIO_PULL_UP);
+	s3c_gpio_setpull(S3C64XX_GPB(3), S3C_GPIO_PULL_UP);
 }
